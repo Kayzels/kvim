045bcf51664ff2bf7563324fb27d67603b07f127
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-07-19 17:16:15 +0200

    [noice]: Disable command palette preset
    This leads to weird tab complete, where the preset expects the command
    line to be floating in the center, but it is instead at the bottom left.
    Disabling this allows blink.cmp to be used for the cmdline instead,
    which shows up in the right place.

--- lua/kayzels/plugins/ui/init.lua
+++ lua/kayzels/plugins/ui/init.lua
@@ -33,7 +33,7 @@ return {
       },
       presets = {
         bottom_search = true,
-        command_palette = true,
+        -- command_palette = true,
         long_message_to_split = true,
         lsp_doc_border = true,
       },
