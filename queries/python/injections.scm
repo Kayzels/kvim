d3b03635c80064ffd06d57b1dbe0ed013eb53ea2
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-10-11 10:41:24 +0200

    feat: Add SQL highlighting to Python

---
+++ queries/python/injections.scm
@@ -0,0 +1,9 @@
+; extends
+
+(
+  [
+    (string_content)
+  ] @injection.content
+  (#match? @injection.content "\^w*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER.*$")
+  (#set! injection.language "sql")
+)
