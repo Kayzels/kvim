9699df46457c331fa2dd3addf2b92b6476591a6a
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2025-10-15 09:10:01 +0200

    feat(sql): Add JOIN as highlight injection word

--- queries/python/injections.scm
+++ queries/python/injections.scm
@@ -4,6 +4,6 @@
   [
     (string_content)
   ] @injection.content
-  (#match? @injection.content "\^w*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER.*$")
+  (#match? @injection.content "\^w*SELECT|FROM|INNER JOIN|JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER.*$")
   (#set! injection.language "sql")
 )
