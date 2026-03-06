48e284eeb1b5a6a0529b5e8a2827cbf170f60c74
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2026-03-06 08:37:15 +0200

    fix(lsp): Correct typo for clangd arguments

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -28,7 +28,7 @@ return {
     "--clang-tidy",
     "--header-insertion=iwyu",
     "--completion-style=detailed",
-    "--functon-arg-placeholders",
+    "--function-arg-placeholders",
     "--fallback-style=llvm",
   },
   filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
