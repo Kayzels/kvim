94a840f6af15d537d6413f21ea4574a5d53f5128
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2026-04-09 14:54:12 +0200

    feat(qml): Add some if statement indentation

--- after/queries/qmljs/indents.scm
+++ after/queries/qmljs/indents.scm
@@ -6,3 +6,13 @@
 (statement_block "}" @indent.branch @indent.end)
 (ui_object_initializer "}" @indent.branch @indent.end)
 
+(else_clause) @indent.branch
+
+(statement_block
+  (if_statement) . (ERROR "else") @indent.begin)
+
+(if_statement
+  consequence: (_) @indent.dedent
+  (#not-has-parent? @indent.begin "else_clause")
+  (#not-kind-eq? @indent.dedent "statement_block")) @indent.begin
+
