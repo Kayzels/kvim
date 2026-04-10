23d71ae1b77a88cf54cb1807984fe961aabdd03a
Kyzan Hartwig <72696811+Kayzels@users.noreply.github.com>
2026-04-10 14:11:35 +0200

    fix(qml): Get indentation working for if/else properly

--- after/queries/qmljs/indents.scm
+++ after/queries/qmljs/indents.scm
@@ -1,18 +1,20 @@
 [
   (ui_object_initializer)
   (statement_block)
+  (if_statement)
 ] @indent.begin
 
 (statement_block "}" @indent.branch @indent.end)
 (ui_object_initializer "}" @indent.branch @indent.end)
 
-(else_clause) @indent.branch
-
-(statement_block
-  (if_statement) . (ERROR "else") @indent.begin)
+(else_clause "else") @indent.branch
+(else_clause [(statement_block) (if_statement)] @indent.dedent)
+(else_clause (if_statement consequence: (statement_block) @indent.dedent))
 
 (if_statement
   consequence: (_) @indent.dedent
-  (#not-has-parent? @indent.begin "else_clause")
-  (#not-kind-eq? @indent.dedent "statement_block")) @indent.begin
+  (#not-kind-eq? @indent.dedent statement_block))
+
+(statement_block
+  (if_statement) . (ERROR "else") @indent.begin)
 
